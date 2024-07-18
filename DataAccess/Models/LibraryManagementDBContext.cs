df3096617805f4dcc48fa99c3a0b434e7c5af1eb
nguyenthanhlam20 <nguyenthanhlam1070@gmail.com>
2024-07-19 06:59:16 +0700

    .

--- DataAccess/Models/LibraryManagementDBContext.cs
+++ DataAccess/Models/LibraryManagementDBContext.cs
@@ -21,12 +21,15 @@ namespace DataAccess.Models
         public virtual DbSet<History> Histories { get; set; } = null!;
         public virtual DbSet<Student> Students { get; set; } = null!;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+       protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
+              if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server=localhost;database=LibraryManagementDB;user=sa;password=55555;TrustServerCertificate=true");
+                var builder = new ConfigurationBuilder()
+                                 .SetBasePath(Directory.GetCurrentDirectory())
+                                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                IConfigurationRoot configuration = builder.Build();
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("DB"));
             }
         }
 
